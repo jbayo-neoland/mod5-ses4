0c9a5720f74f81ce0af110abd8737d46f0d28d3c
Jordi <jordi@iceverg.com>
2021-03-26 18:57:37 +0100

    kickoff Unit Testing

---
+++ prueba/demo.test.js
@@ -0,0 +1,17 @@
+
+describe('demo', () => {
+  describe('tests', () => {
+
+    test('first demo test', () => {
+
+    })
+
+    it('second demo test', () => {
+
+    })
+  })
+
+  it('superprueba', () => {
+    
+  })
+})
---
+++ prueba/error.test.js
@@ -0,0 +1,7 @@
+test('peta!', () => {
+  expect(false).toBe(false);
+  expect(1).toBe(1);
+  expect(10).toBe(10);
+  expect(true).toBeTruthy();
+  expect(1).toEqual(10);
+})
