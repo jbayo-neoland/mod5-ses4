a11f63664732c01849c457c17a9f0c4a5b86ec36
LCLAR <luisclarprohens@gmail.com>
2021-03-26 20:16:13 +0100

    missing 1 file to upload

--- __tests__/carrito.test.js
+++ __tests__/carrito.test.js
@@ -10,3 +10,46 @@ describe('Testing Carrito class', () => {
 
   
 })
+
+describe('Testing addProduct', () => {
+  it('should add a product to the cart', () => {
+    let carrito = new Carrito();
+    carrito.addProduct({'item': 'agua', 'price': 10})
+    expect(carrito.items).toContainEqual({'item':'agua', 'price': 10})
+  })
+})
+
+describe('Testing removeProduct', () => {
+  it('should remove a product from the cart', () => {
+    let carrito = new Carrito();
+    carrito.addProduct({'item': 'agua', 'price': 10})
+    carrito.removeProduct({'item': 'agua', 'price': 10})
+    expect(carrito.items.length).toBe(0)
+  })
+})
+
+describe('Testing getTotalItems', () => {
+  it('should return all products from the cart', () => {
+    let carrito = new Carrito();
+    carrito.addProduct({'item': 'agua', 'price': 10})
+    carrito.addProduct({'item': 'pizza', 'price': 20})
+    var total = carrito.getTotalItems()
+    expect(total).toEqual(2)
+  })
+})
+
+describe('Testing getTotalItems', () => {
+  it('should return all products from the cart', () => {
+    let carrito = new Carrito();
+    carrito.addProduct({'item': 'agua', 'price': 10})
+    carrito.addProduct({'item': 'pizza', 'price': 20})
+    let checkout = 0;
+    for (let index = 0; index < carrito.items.length; index++) {
+      checkout = checkout + carrito.items[index].price;
+    }
+    const totalprecio = carrito.getTotalCheckout();
+
+    expect(totalprecio).toEqual(checkout)
+  })
+})
+
